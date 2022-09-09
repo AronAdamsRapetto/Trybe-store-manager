3c0e781c4dd4a7e36f6c449e0d7590454346b0f1
Aron Adams <aronrapetto@hotmail.com>
2022-09-09 19:08:44 -0300

    fix: tratamento de erro corrigido

--- src/services/validations/validationNameSchema.js
+++ src/services/validations/validationNameSchema.js
@@ -2,7 +2,8 @@ const { nameSchema } = require('./schemas');
 
 const validationNameSchema = (name) => {
   const { error } = nameSchema.validate(name);
-  if (error) return { type: 'INVALID_FIELD', message: error.message };
+  if (error.message.includes('required')) return { type: 'INVALID_FIELD', message: error.message };
+  if (error.message.includes('length')) return { type: 'INVALID_VALUE', message: error.message };
   return { type: null, message: '' };
 };
 
--- src/utils/errorMap.js
+++ src/utils/errorMap.js
@@ -1,5 +1,7 @@
 const errorMap = {
   NOT_FOUND: 404,
+  INVALID_FIELD: 400,
+  INVALID_VALUE: 422,
 };
 
 const errorCode = (error) => errorMap[error];
