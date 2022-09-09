f092a77795c46c77a36a810976eaa838ad32ffad
Aron Adams <aronrapetto@hotmail.com>
2022-09-09 19:49:04 -0300

    fix: schema corrigido, req 3 e 4 concluidos

--- src/services/validations/validationNameSchema.js
+++ src/services/validations/validationNameSchema.js
@@ -2,8 +2,16 @@ const { nameSchema } = require('./schemas');
 
 const validationNameSchema = (name) => {
   const { error } = nameSchema.validate(name);
-  if (error.message.includes('required')) return { type: 'INVALID_FIELD', message: error.message };
-  if (error.message.includes('length')) return { type: 'INVALID_VALUE', message: error.message };
+  console.log(error);
+  if (error) {
+    return {
+      type: error.message.includes('required')
+        ? 'INVALID_FIELD'
+        : 'INVALID_VALUE',
+      message: error.message,
+    }; 
+  } 
+
   return { type: null, message: '' };
 };
 
