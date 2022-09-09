1396d7c8da6a5756af5696b093a935e23f7df46f
Aron Adams <aronrapetto@hotmail.com>
2022-09-09 17:19:31 -0300

    feat: productService adicionado, tratamentos implementados

---
+++ src/services/product.service.js
@@ -0,0 +1,18 @@
+const { productModel } = require('../models');
+
+const getAllProducts = async () => {
+  const products = await productModel.getAllProducts();
+  return products;
+};
+
+const getProductById = async (id) => {
+  const product = await productModel.getProductById(id);
+
+  if (product) return { type: null, message: product };
+  return { type: 'NOT_FOUND', message: 'Product not found' };
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
