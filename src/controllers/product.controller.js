5f0b0fcde0168de4cfb2fda85864ce82248c77ad
Aron Adams <aronrapetto@hotmail.com>
2022-09-09 19:01:49 -0300

    feat: implementada rota POST /products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -15,7 +15,16 @@ const getProductById = async (req, res) => {
   return res.status(200).json(message);
 };
 
+const insertProduct = async (req, res) => {
+  const { name } = req.body;
+  const { type, message } = await productService.insertProduct(name);
+
+  if (type) return res.status(errorCode(type)).json({ message });
+  return res.status(201).json(message);
+};
+
 module.exports = {
   getAllProducts,
   getProductById,
+  insertProduct,
 };
