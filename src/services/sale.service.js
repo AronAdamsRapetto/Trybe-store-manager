00da0935b0ab8cbf9f173e01e5b0929c79bad4fd
Aron Adams <aronrapetto@hotmail.com>
2022-09-11 22:51:16 -0300

    feat: saleService.updateSale

--- src/services/sale.service.js
+++ src/services/sale.service.js
@@ -44,9 +44,26 @@ const removeSale = async (id) => {
   return { type: null, message: '' };
 };
 
+const updateSale = async (id, sales) => {
+  const validation = validationSaleSchema(sales);
+  if (validation.type) return validation;
+
+  const validationSale = await getSaleById(id);
+  if (validationSale.type) return validationSale;
+
+  const someProductNotExist = await verifyProducts(sales);
+  if (someProductNotExist) return { type: 'NOT_FOUND', message: 'Product not found' };
+
+  await Promise.all(sales
+    .map(async ({ productId, quantity }) => saleModel.updateSale(id, productId, quantity)));
+  
+  return { type: null, message: { saleId: id, itemsUpdated: sales } };
+};
+
 module.exports = {
   insertSale,
   getAllSales,
   getSaleById,
   removeSale,
+  updateSale,
 };
